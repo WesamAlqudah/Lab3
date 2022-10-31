1610ebef37593c0557073a2357cebbe072ff1925
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-10-30 21:41:51 -0500

    Lab 3

---
+++ app/src/main/java/com/cs473de/lab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cs473de.lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/cs473de/lab/TableLayotActive.kt
@@ -0,0 +1,80 @@
+package com.cs473de.lab
+
+import android.os.Bundle
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.cs473de.lab.databinding.ActivityMainBinding
+import com.cs473de.lab.databinding.ActivityMainBinding.inflate
+
+class TableLayotActive : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnAddRow.setOnClickListener {
+            if (validateFields()) {
+                addNewRow(
+                    binding.edAndroidVersion.text.toString(),
+                    binding.edAndroidCodeName.text.toString()
+                )
+            }
+        }
+
+    }
+
+    private fun validateFields(): Boolean {
+
+        if (binding.edAndroidVersion.text.isEmpty()) {
+            Toast.makeText(this, "Enter android code version please", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (binding.edAndroidCodeName.text.isEmpty()) {
+            Toast.makeText(this, "Enter android code name please", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
+    private fun addNewRow(version: String, codeName: String) {
+
+        // Create a new table row.
+        val tableRow = TableRow(applicationContext)
+
+        // Set new table row layout parameters.
+        val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+        tableRow.layoutParams = layoutParams
+
+        // add values into row by calling addView()
+        val tvVersion = TextView(this, null, 0, R.style.tableCell)
+        val tvCode = TextView(this, null, 0, R.style.tableCell)
+
+        tableRow.addView(tvVersion, 0)
+        tableRow.addView(tvCode, 1)
+
+        //set margin
+        val lp = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        lp.setMargins(5, 5, 5, 5)
+        tvVersion.layoutParams = lp
+        tvCode.layoutParams = lp
+
+        //set values
+        tvVersion.text = version
+        tvCode.text = codeName
+
+
+        // Finally add the created row row into layout
+        binding.tblAndroidData.addView(tableRow)
+
+    }
+}
